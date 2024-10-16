ee2866a1b18ba8b2da2848e6f855c001d3e221f5
Thammanoon Semapru <samutpra@hotmail.com>
2024-10-16 10:14:40 +0700

    rename mock storeLocation

--- backend/lib/mocks.ts
+++ backend/lib/mocks.ts
@@ -1,5 +1,5 @@
 export * from 'lib/mocks/mock_currency';
 export * from 'lib/mocks/mock_exchangerate';
 export * from 'lib/mocks/mock_product';
-export * from 'lib/mocks/mock_storeLocation';
+export * from 'lib/mocks/mock_storelocation';
 export * from 'lib/mocks/mock_tenant';
